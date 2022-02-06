8771c9c00c3e1d89c1e1f67caeb1dc37402e5633
yanomaly <family.tolstoy@gmail.com>
2022-02-06 13:21:04 +0300

    Refactor main unlog

--- src/main/java/com/example/WebApplication/controllers/MainUnlogController.java
+++ src/main/java/com/example/WebApplication/controllers/MainUnlogController.java
@@ -1,13 +1,10 @@
 package com.example.WebApplication.controllers;
-import com.example.WebApplication.entities.Ad;
 import com.example.WebApplication.service.AdService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -19,6 +16,6 @@ public class MainUnlogController {
     @GetMapping
     public String mainPage(Model model){
         model.addAttribute("adds", adService.getThreeLast());
-        return "main_unlog";
+        return "index";
     }
 }
